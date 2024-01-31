d9cab95f075e77b5a8fc93da8035223c1d0f4eb2
Jeremy Chambon <jeremy.chambon2906@gmail.com>
2024-01-31 15:33:56 +0100

    ajout fondu noir pour section 2

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
-// Attendez que le DOM soit chargé
 document.addEventListener("DOMContentLoaded", function () {
-    // Sélectionnez l'élément du titre et de la lune
+
     const title = document.querySelector(".title");
     const moon = document.querySelector(".moon img");
+    const blackOverlay = document.querySelector(".black-overlay");
 
-    // Utilisez GSAP pour créer l'animation
     gsap.registerPlugin(ScrollTrigger);
 
-    // Créez l'animation avec ScrollTrigger pour le titre
     gsap.to(title, {
         scrollTrigger: {
-            trigger: ".all", // Le déclencheur de l'animation
-            start: "0%", // Où l'animation commence par rapport au déclencheur
-            end: "35%", // Où l'animation se termine par rapport au déclencheur
-            scrub: 0.5, // Pour un défilement plus doux
+            trigger: ".all",
+            start: "0%",
+            end: "35%",
+            scrub: 0.5,
         },
-        fontSize: "2rem", // Taille de police réduite
-        opacity: 0, // Opacité réduite à 0 pour disparition complète
+        fontSize: "2rem",
+        opacity: 0,
     });
 
-    // Créez l'animation avec ScrollTrigger pour la lune
     gsap.to(moon, {
         scrollTrigger: {
-            trigger: ".all", // Le déclencheur de l'animation
-            start: "-5%", // Où l'animation commence par rapport au déclencheur
-            end: "50%", // Où l'animation se termine par rapport au déclencheur
-            scrub: 0.5, // Pour un défilement plus doux
+            trigger: ".all",
+            start: "-5%",
+            end: "50%",
+            scrub: 0.5,
             markers: true,
         },
-        scale: 5, // Facteur de zoom
+        scale: 5,
     });
 
-    // Ajoutez d'autres étapes d'animation au besoin
+    gsap.to(blackOverlay, {
+        scrollTrigger: {
+            trigger: ".all",
+            start: "20%",
+            end: "50%",
+            scrub: 0.5,
+            markers: true,
+        },
+        opacity: 1,
+    });
 
 });
