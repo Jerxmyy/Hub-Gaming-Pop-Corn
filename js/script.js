ae98a93ac2c4678ca4cab3108c41dbc2a1573fd3
Jerxmyy <151178814+Jerxmyy@users.noreply.github.com>
2024-02-01 10:15:15 +0100

    ajout fondu couleur pour scroll

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
             trigger: ".all",
             start: "-5%",
             end: "50%",
-            scrub: 0.5,
+            scrub: 2,
         },
         scale: 5,
     });
@@ -35,6 +35,77 @@ document.addEventListener("DOMContentLoaded", function () {
             scrub: 0.5,
         },
         opacity: 1,
+    });
+
+    gsap.to(blackOverlay, {
+        scrollTrigger: {
+            trigger: ".all",
+            start: "50%",
+            end: "65%",
+            scrub: 0.5,
+        },
+        backgroundColor: "red",
+    });
+
+    gsap.to(blackOverlay, {
+        scrollTrigger: {
+            trigger: ".all",
+            start: "65%",
+            end: "80%",
+            scrub: 0.5,
+        },
+        backgroundColor: "blue",
+    });
+
+    gsap.to(blackOverlay, {
+        scrollTrigger: {
+            trigger: ".all",
+            start: "80%",
+            end: "95%",
+            scrub: 0.5,
+        },
+        backgroundColor: "green",
+    });
+
+    gsap.to(blackOverlay, {
+        scrollTrigger: {
+            trigger: ".all",
+            start: "95%",
+            end: "110%",
+            scrub: 0.5,
+        },
+        backgroundColor: "yellow",
+    });
+
+    gsap.to(blackOverlay, {
+        scrollTrigger: {
+            trigger: ".all",
+            start: "110%",
+            end: "125%",
+            scrub: 0.5,
+        },
+        backgroundColor: "purple",
+    });
+
+    gsap.to(blackOverlay, {
+        scrollTrigger: {
+            trigger: ".all",
+            start: "125%",
+            end: "140%",
+            scrub: 0.5,
+        },
+        backgroundColor: "pink",
+    });
+
+    gsap.to(".all", {
+        scrollTrigger: {
+            trigger: ".all",
+            start: "bottom bottom",
+            end: "+=200%",
+            scrub: 0.5,
+            markers: true,
+        },
+        height: "300vh",
     });
 
 });
